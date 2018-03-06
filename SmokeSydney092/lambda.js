d2867271bec80f327215f54c19d2288875e31bc4
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-03-06 13:46:15 +0530

    name changed

--- SmokeSydney092/lambda.js
+++ SmokeSydney092/lambda.js
@@ -3,7 +3,7 @@ let SL = require('@slappforge/slappforge-sdk');
 const sqs = new SL.AWS.SQS(AWS);
 exports.handler = function (event, context, callback) {
 	sqs.receiveMessage({
-		QueueUrl: 'https://sqs.ap-southeast-2.amazonaws.com/263248768798/test',
+		QueueUrl: 'https://sqs.ap-southeast-2.amazonaws.com/263248768798/sample',
 		AttributeNames: ['All'],
 		MaxNumberOfMessages: '1',
 		VisibilityTimeout: '30',
