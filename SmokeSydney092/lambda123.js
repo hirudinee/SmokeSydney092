af60d95465976d8e1cde8b5111cc915c5cd416bb
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-03-06 16:25:40 +0530

    test 123

--- SmokeSydney092/lambda123.js
+++ SmokeSydney092/lambda123.js
@@ -1,6 +1,8 @@
 let AWS = require('aws-sdk');
 let SL = require('@slappforge/slappforge-sdk');
 const sqs = new SL.AWS.SQS(AWS);
+exports.handler = function (event, context, callback) {
+	
 
 
 	callback(null, 'Successfully executed');
