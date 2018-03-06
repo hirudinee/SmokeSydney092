758902cf4261f1948fad50b73240cfa14fd578ee
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-03-06 17:08:15 +0530

    sample

--- SmokeSydney092/lambda123.js
+++ SmokeSydney092/lambda123.js
@@ -1,9 +1,24 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 let SL = require('@slappforge/slappforge-sdk');
 const sqs = new SL.AWS.SQS(AWS);
 exports.handler = function (event, context, callback) {
-	
-
 
 	callback(null, 'Successfully executed');
+
+	ddb.put({
+		TableName: 'Test',
+		Item: {
+			'key': 'value'
+		}
+	}, function (err, data) {
+		if (err) {
+			//handle error
+		} else {
+			//your logic goes here
+		}
+	});
+
+
+
 }
