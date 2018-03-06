040d7cb4c0d64c85b4f9ab8ed949a2d1f136e641
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-03-06 16:18:47 +0530

    test

---
+++ SmokeSydney092/lambda123.js
@@ -0,0 +1,22 @@
+let AWS = require('aws-sdk');
+let SL = require('@slappforge/slappforge-sdk');
+const sqs = new SL.AWS.SQS(AWS);
+exports.handler = function (event, context, callback) {
+	sqs.receiveMessage({
+		QueueUrl: 'https://sqs.ap-southeast-2.amazonaws.com/263248768798/sample',
+		AttributeNames: ['All'],
+		MaxNumberOfMessages: '1',
+		VisibilityTimeout: '30',
+		WaitTimeSeconds: '0'
+	}, function (receivedMessages) {
+		receivedMessages.forEach(message => {
+			// your logic to access each message through out the loop. Each message is available under variable message 
+			// within this block
+		})
+	}, function (error) {
+		// implement error handling logic here
+	});
+
+
+	callback(null, 'Successfully executed');
+}
